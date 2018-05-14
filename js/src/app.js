6d229108b1a7b816d5cae1d4494aaa81633d7f5e
Franco <franverri@hotmail.com>
2018-05-13 22:51:27 -0300

    Endpoints finalizados

--- js/src/app.js
+++ js/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+function sleep(time, callback) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+    callback();
+}
+
 const app = express();
 
 var PORT = process.argv[2];
@@ -7,6 +15,20 @@ var PORT = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send("Hi, I'm root node");
+});
+
+app.get('/sleep', (req, res) => {
+  sleep(5000, function() {
+     res.send("Node Sleep Test");
+  })
+});
+
+app.get('/cycle', (req, res) => {
+  var i = 0;
+  while (i < 100000000) {
+    i++;
+  }
+  res.send("Node Cycle Test");
 });
 
 // Start server
--- py/app.py
+++ py/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return 'Hi, Im root py'
+
+@app.route("/sleep")
+def sleep():
+    time.sleep(5)
+    return 'Python Sleep Test'
+
+@app.route("/cycle")
+def cycle():
+    i = 0
+    while (i < 100000000):
+      i += 1
+    return 'Python Cycle Test'
 
 
 if __name__ == "__main__":
